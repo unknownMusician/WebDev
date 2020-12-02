0f30c45754b696b5e5425c805585d57260e0e040
nekt2111 <54265542+nekt2111@users.noreply.github.com>
2020-12-02 20:42:29 +0200

    123

--- WebSites/directoryRadioFind.php
+++ WebSites/directoryRadioFind.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli("127.0.0.1", "root", "12345", "pharmacy");
+$conn = new mysqli("127.0.0.1", "root", "12345", "radiodirectory");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -19,45 +19,65 @@ $radioEngineeringName = $_POST["radioEngineering-name"];
 $radioEngineeringDescription = $_POST["radioEngineering-description"];
 $radioEngineeringPrice = $_POST["radioEngineering-price"];
 
-
 $query = "SELECT
-products.id as 'ID',
-products.name as 'Назва продукту', 
-products.description as 'Опис продукту', 
-products.price as 'Ціна продукту',
-clients.name as 'Ім`я клієнта',
-clients.surname as 'Прізвище клієнта',
-clients.middlename as 'По-батькові клієнта',
-workers.name as 'Ім`я працівника',
-workers.surname as 'Прізвище працівника',
-workers.middlename as 'По-батькові працівника'
-FROM products, clients, workers, deals
+chapters.id as 'ID',
+chapters.name as 'Назва глави', 
+chapters.amountPages as 'Кількість сторінок глави', 
+chapters.epigraph as 'Епіграф глави',
+paragraphs.name as 'Назва параграфа',
+paragraphs.epigraph as 'Епіграф параграфа',
+paragraphs.amountPages as 'Кількість сторінок параграфа',
+radioengineering.name as 'Назва радіоприладу',
+radioengineering.description as 'Опис',
+radioengineering.price as 'Ціна'
+FROM chapters, paragraphs, radioengineering, paragraphradio
 WHERE 
-deals.idclient = clients.id AND
-deals.idworker = workers.id AND
-deals.idproduct = products.id"; // todo add search;
 
-if ($clientName != ""){
-    $query = $query . " AND clients.name = '$clientName'";
-} if ($productPrice != ""){
-    $query = $query . " AND products.price = $productPrice";
+paragraphs.idChapter = chapters.id AND 
+paragraphradio.idParagraph = paragraphs.id AND 
+paragraphradio.idRadioEngr = radioengineering.id";
+
+
+if ($chapterName != ""){
+    $query = $query . " AND chapters.name = '$chapterName'";
+} if ($chapterAmountPages != ""){
+    $query = $query . " AND chapters.amountPages = $chapterAmountPages";
+} if ($chapterEpigraph != ""){
+    $query = $query . " AND chapters.epigraph = '$chapterEpigraph'";
+} if ($paragraphName != ""){
+    $query = $query . " AND paragraphs.name = '$paragraphName'";
+} if ($paragraphEpigraph != ""){
+    $query = $query . " AND paragraphs.epigraph = '$paragraphEpigraph'";
+} if ($paragraphAmountPages != ""){
+    $query = $query . " AND paragraphs.amountPages = $paragraphAmountPages";
+} if ($radioEngineeringName != ""){
+    $query = $query . " AND radioengineering.name = '$radioEngineeringName'";
+} if ($radioEngineeringDescription != ""){
+    $query = $query . " AND radioengineering.description = '$radioEngineeringDescription'";
+} if ($radioEngineeringPrice != ""){
+    $query = $query . " AND radioengineering.price = $radioEngineeringPrice ";
 }
 
 $result = $conn->query($query);
+
+if (!$result) {
+    trigger_error('Invalid query: ' . $conn->error);
+}
 
 if ($result->num_rows > 0) {
     echo "<style>td {padding: 10px}</style>";
     echo "<table border='1px'>";
     echo "<tr>";
-    echo "<td>ID</td><td>Назва продукту</td><td>Опис продукту</td>
-    <td>Ціна продукту</td><td>Ім`я клієнта</td><td>Прізвище клієнта</td>
-    <td>По-батькові клієнта</td><td>Ім`я працівника</td>
-    <td>Прізвище працівника</td><td>По-батькові працівника</td>";
+    echo "<td>ID</td><td>Назва глави</td><td>Кількість сторінок глави</td>
+    <td>Епіграф глави</td><td>Назва параграфа</td><td>Епіграф параграфа</td>
+    <td>Кількість сторінок параграфа</td><td>Назва радіоприладу</td>
+    <td>Опис</td><td>Ціна</td>";
     echo "</tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<tr>";
         //
+        implode($row);
         foreach($row as $part){
             echo "<td>";
             echo $part;
