1ab9ce9ac0c086abd65afbbee451f001b12c6922
nekt2111 <54265542+nekt2111@users.noreply.github.com>
2020-12-03 12:38:59 +0200

    12321

---
+++ lab7/sql.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE radioDirectory DEFAULT CHARACTER SET utf8;
+USE radioDirectory;
+
+CREATE TABLE chapters (id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                      name VARCHAR(30) NOT NULL UNIQUE KEY,
+                      amountPages SMALLINT UNSIGNED NOT NULL,
+                      epigraph TEXT(1000) NULL
+                      )ENGINE = INNODB;
+
+CREATE TABLE paragraphs (id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                         name VARCHAR(30) NOT NULL UNIQUE KEY,
+                         idChapter SMALLINT UNSIGNED NOT NULL, FOREIGN KEY (idChapter) REFERENCES chapters(id) ON DELETE CASCADE ON UPDATE CASCADE,
+                         epigraph TEXT(1000) NULL,
+                         amountPages SMALLINT UNSIGNED NOT NULL)
+                         ENGINE=INNODB;
+
+CREATE TABLE radioEngineering (id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                               name VARCHAR(30) NOT NULL UNIQUE KEY,
+                               description TEXT(65335) NOT NULL,
+                               price SMALLINT UNSIGNED NOT NULL)
+                               ENGINE=INNODB;
+
+CREATE TABLE paragraphRadio (id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                             idParagraph SMALLINT UNSIGNED NOT NULL,FOREIGN KEY (idParagraph) REFERENCES paragraphs(id) ON  DELETE CASCADE ON UPDATE CASCADE,
+                             idRadioEngr SMALLINT UNSIGNED NOT NULL,
+                             FOREIGN KEY (idRadioEngr) REFERENCES radioEngineering(id) ON  DELETE CASCADE ON UPDATE CASCADE)ENGINE=INNODB;
